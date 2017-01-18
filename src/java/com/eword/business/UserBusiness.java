b98565e7feb560ec1ff6a62e2135bc4b3dc93505
JG <jean.gabriel089@gmail.com>
2017-01-18 20:22:11 +0100

    Creation of UserBusiness

---
+++ src/java/com/eword/business/UserBusiness.java
@@ -0,0 +1,31 @@
+package com.eword.business;
+
+import com.eword.beans.User;
+import com.eword.dao.DAOFactory;
+import com.eword.dao.interfaces.UserDAO;
+import javax.servlet.http.HttpServletRequest;
+
+public class UserBusiness {
+
+    /**
+     * Object enabling to communicate with the User data layer
+     */
+    private static final UserDAO USER_DAO = DAOFactory.getInstance().getUserDAO();
+
+    /**
+     * Return the User corresponding to the username and the password if they
+     * exist.
+     *
+     * @param req The request
+     * @return The User if they exist. Null otherwise
+     */
+    public static User getUser(HttpServletRequest req) {
+        //We retrieve the parameters of the request to identify the user
+        String username = req.getParameter("username");
+        String password = SignupForm.sha256(req.getParameter("password"));
+
+        User user = USER_DAO.find(username, password);
+
+        return user;
+    }
+}
