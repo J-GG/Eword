77ab942f848e5489948b7622e6c5d02e73ff2e39
JG <jean.gabriel089@gmail.com>
2017-01-18 21:04:50 +0100

    Added language check when accessing the servlet

--- src/java/com/eword/servlets/LanguageSelection.java
+++ src/java/com/eword/servlets/LanguageSelection.java
@@ -1,6 +1,7 @@
 package com.eword.servlets;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import javax.servlet.ServletException;
@@ -16,25 +17,34 @@ public class LanguageSelection extends HttpServlet {
      */
     private final static String ATT_LANG = "language";
 
+    private final static String[] LANGUAGES = {"us", "fr"};
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
         //Creation of the path for the redirection. It is either the previous page or the home page
         String address = req.getHeader("referer");
+        String context = req.getContextPath();
         if (address == null) {
-            address = req.getContextPath();
+            address = context;
         }
 
         //Verification that the URL is correct. if not, the user is redirected
-        String context = req.getContextPath();
         Matcher matcher = Pattern.compile(context + "/lang/([a-z]{2})$").matcher(req.getRequestURI());
 
         if (!matcher.find()) {
             resp.sendRedirect(address);
+            return;
         }
 
         //The language is extracted from the URL and inserted into a request attribute
         String lang = matcher.group(1);
+
+        //If the extracted language doesn't belong to the list, the user is redirected
+        if (!Arrays.asList(LANGUAGES).contains(lang)) {
+            resp.sendRedirect(address);
+            return;
+        }
 
         HttpSession session = req.getSession();
         session.setAttribute(ATT_LANG, lang);
